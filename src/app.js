7a942fd4f03f3420a72e6c71a70b19fbd67a5b8c
Abhiram B.S.N <abhirambsn@gmail.com>
2023-02-17 22:42:25 +0530

    Health Check Added

--- src/app.js
+++ src/app.js
@@ -23,4 +23,8 @@ app.use('/api/account', accountRoutes);
 
 app.set('port', PORT);
 
+app.get('/health', (req, res) => {
+    return res.status(200).json({code: 200, status: "OK"})
+})
+
 module.exports = app;
